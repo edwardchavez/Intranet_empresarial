96518e4efc19fb167a30f5bca4f7498898b714f8
edwardchavez <cruzchavezedward@gmail.com>
2017-12-20 10:03:42 -0400

    trabajando la parte de argar el excel

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express.Router();
+var XLSX = require('xlsx');
 
 var db = require('../db/db');
 
@@ -157,10 +158,55 @@ app.get('/lectorexcel', function (req, res, next) {
   res.render('lectorexcel.ejs');
 });
 
-app.post('/lectorexcel', function(req, res){
-var info;
+app.post('/lectorexcel', function(req, res, next){
 
-/*console.log(info);*/
+if (req.method == "POST"){
+  var info = req.query;  
+  //var tabla = info.titulo;
+  
+  console.log(info);
+}else{
+  console.log("error");
+}
+
+
+
+//var XLSX = require('xlsx');
+/*var workbook = XLSX.readFile(info);
+var sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(function(y) {
+    var worksheet = workbook.Sheets[y];
+    var headers = {};
+    var data = [];
+    for(z in worksheet) {
+        if(z[0] === '!') continue;
+        //parse out the column, row, and value
+        var tt = 0;
+        for (var i = 0; i < z.length; i++) {
+            if (!isNaN(z[i])) {
+                tt = i;
+                break;
+            }
+        };
+        var col = z.substring(0,tt);
+        var row = parseInt(z.substring(tt));
+        var value = worksheet[z].v;
+
+        //store header names
+        if(row == 1 && value) {
+            headers[col] = value;
+            continue;
+        }
+
+        if(!data[row]) data[row]={};
+        data[row][headers[col]] = value;
+    }
+    //drop those first two rows which are empty
+    data.shift();
+    data.shift();
+    console.log(data);
+});
+*/
 
 });
 
