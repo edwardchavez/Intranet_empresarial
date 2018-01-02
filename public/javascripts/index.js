880a00f15ee64aefd8836d4b1d643cfd7cab8d74
edwardchavez <cruzchavezedward@gmail.com>
2018-01-02 15:54:14 -0400

    cambiando configuracion en el login

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,11 +14,10 @@ function validate(){
   var n1 = document.getElementById("user_name");
   var n2 = document.getElementById("password");
   if(n1.value!="" && n2.value!=""){
+    return true;
+  }else{
     alert("Debe completar todos los campos...");
     return false;
-  }else{
-   return true;
-
   }
 }
 
