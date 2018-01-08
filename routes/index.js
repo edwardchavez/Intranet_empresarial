c6b9099a0337db37d00fd71ed433f150db78d8fd
edwardchavez <cruzchavezedward@gmail.com>
2018-01-08 17:40:12 -0400

    trabajando en la reserva del salon

--- routes/index.js
+++ routes/index.js
@@ -156,66 +156,10 @@ app.post('/editprofile',function(req,res){
 });
 
 
-//-----------------------------------------lector excel-------------------------------------------//
+//-----------------------------------------Reserva salon-------------------------------------------//
 
-app.get('/lectorexcel', function (req, res, next) {
-  res.render('lectorexcel.ejs');
-});
-
-app.post('/lectorexcel', function(req, res, next){
-
-//console.log(req.session);
-
-if (req.method == "POST"){
-  var info = req.body; 
-
-  //var tabla = info.titulo;
-  
-  console.log(info);
-}else{
-  console.log("error");
-}
-
-
-
-//var XLSX = require('xlsx');
-/*var workbook = XLSX.readFile(info);
-var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) {
-    var worksheet = workbook.Sheets[y];
-    var headers = {};
-    var data = [];
-    for(z in worksheet) {
-        if(z[0] === '!') continue;
-        //parse out the column, row, and value
-        var tt = 0;
-        for (var i = 0; i < z.length; i++) {
-            if (!isNaN(z[i])) {
-                tt = i;
-                break;
-            }
-        };
-        var col = z.substring(0,tt);
-        var row = parseInt(z.substring(tt));
-        var value = worksheet[z].v;
-
-        //store header names
-        if(row == 1 && value) {
-            headers[col] = value;
-            continue;
-        }
-
-        if(!data[row]) data[row]={};
-        data[row][headers[col]] = value;
-    }
-    //drop those first two rows which are empty
-    data.shift();
-    data.shift();
-    console.log(data);
-});
-*/
-
-});
-
+app.get ('/reservasalon', function(req,res){
+  res.render("salon");
+})
 
 module.exports = app;
